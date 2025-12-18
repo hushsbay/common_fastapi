c7da5214ff373fb4db7ae175e2a2b96a512a40d9
hushsbay <hushsbay@gmail.com>
2025-12-18 14:10:21 +0900

    .

--- common_fastapi/restful/rqst.py
+++ common_fastapi/restful/rqst.py
@@ -1,7 +1,7 @@
 from pydantic import BaseModel
 from typing import Any, Dict, Optional
 
-class ChatRequest(BaseModel):
+class ChatRequest(BaseModel): # 챗봇에서 요청하는 검색 조건 채우기 또는 실제 검색용 공통 Request
     userid: Optional[str] = None
     text: str
     condition: Optional[Dict[str, Any]] = {}
