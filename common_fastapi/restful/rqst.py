6fc8b65203a2383a8a7d8f212c3b6a2ad55fe3e2
hushsbay <hushsbay@gmail.com>
2025-12-19 14:54:09 +0900

    .

--- common_fastapi/restful/rqst.py
+++ common_fastapi/restful/rqst.py
@@ -6,3 +6,5 @@ class ChatRequest(BaseModel): # 챗봇에서 요청하는 검색 조건 채우�
     text: str
     condition: Optional[Dict[str, Any]] = {}
     search: bool = False
+    embeddingModel: str = 'jhgan'
+    similarityThreshold: float = 0.4
